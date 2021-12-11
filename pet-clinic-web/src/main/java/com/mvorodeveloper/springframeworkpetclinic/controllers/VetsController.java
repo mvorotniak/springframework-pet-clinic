1b965b5b0a0702708fbc5b562b3dc1bca7f0c0a5
mvorotniak <v-mvorotniak@vrbo.com>
2021-12-11 08:10:34 -0800

    List Owners and Vets in index pages. Closes #17 and #18

--- pet-clinic-web/src/main/java/com/mvorodeveloper/springframeworkpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/mvorodeveloper/springframeworkpetclinic/controllers/VetsController.java
@@ -1,14 +1,25 @@
 package com.mvorodeveloper.springframeworkpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mvorodeveloper.springframeworkpetclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
