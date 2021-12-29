2da89c76bc3286b8d0cf5158d8ed6a44475375ce
mvorotniak <v-mvorotniak@vrbo.com>
2021-12-29 03:51:13 -0800

    Convert Vets to JPA Entities. Closes #38

--- pet-clinic-data/src/main/java/com/mvorodeveloper/springframeworkpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/mvorodeveloper/springframeworkpetclinic/model/Vet.java
@@ -3,8 +3,23 @@ package com.mvorodeveloper.springframeworkpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    /**
+     * A table called `vet_specialties` will represent the Vet-Specialty relationship
+     */
+    @ManyToMany
+    @JoinTable(name = "vet_specialties",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
