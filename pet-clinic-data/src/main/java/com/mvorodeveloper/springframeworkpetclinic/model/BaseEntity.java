32dcd9022af7b6d66e97dbaae51325882927ff52
mvorotniak <v-mvorotniak@vrbo.com>
2021-12-29 01:29:48 -0800

    Implement Base Entity JPA. Closes #36

--- pet-clinic-data/src/main/java/com/mvorodeveloper/springframeworkpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/mvorodeveloper/springframeworkpetclinic/model/BaseEntity.java
@@ -2,10 +2,18 @@ package com.mvorodeveloper.springframeworkpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * The Serializable interface is a marker interface;
  * it declares no methods at all. It tells the serialization mechanism that the class can be serialized.
+ *
+ * Annotated with MappedSuperclass, which means we want to inherit from this class
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     /**
@@ -13,6 +21,8 @@ public class BaseEntity implements Serializable {
      * You can't distinguish the default value of a primitive int 0 from an assigned 0
      * while there is no possible ambiguity with a null (a null id always means a new entity)
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
