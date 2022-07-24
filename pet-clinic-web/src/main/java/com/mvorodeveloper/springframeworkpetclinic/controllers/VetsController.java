c19ec39bf95bcd74bb5f0380ce6f46a1259655a4
Mariana Vorotniak <mvorodeveloper@gmail.com>
2022-07-24 00:59:51 -0700

    Added endpoint to return Vets in JSON format

--- pet-clinic-web/src/main/java/com/mvorodeveloper/springframeworkpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/mvorodeveloper/springframeworkpetclinic/controllers/VetsController.java
@@ -1,9 +1,13 @@
 package com.mvorodeveloper.springframeworkpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mvorodeveloper.springframeworkpetclinic.model.Vet;
 import com.mvorodeveloper.springframeworkpetclinic.services.VetService;
 import lombok.AllArgsConstructor;
 
@@ -13,11 +17,19 @@ public class VetsController {
 
     private final VetService vetService;
 
-    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html", "/vets.html"})
-    public String listVets(Model model) {
-        model.addAttribute("vets", vetService.findAll());
+    @GetMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(final Model model) {
+        model.addAttribute("vets", this.vetService.findAll());
 
         return "vets/index";
+    }
+
+    /**
+     * @return List of Vets from the DB in JSON format
+     */
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> listVetsJson() {
+        return this.vetService.findAll();
     }
 
 }
